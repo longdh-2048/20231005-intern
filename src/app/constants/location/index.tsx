3419992f2b3495d4fed37427fc016a7577699a5a
kimha0607 <kimha0607.contact@gmail.com>
2023-10-26 17:54:33 +0700

    feat: list hotel

---
+++ src/app/constants/location/index.tsx
@@ -0,0 +1,36 @@
+interface LocationItem {
+  id: string
+  title: string
+  img: string
+}
+
+export const mainLocation: LocationItem[] = [
+  {
+    id: 'ha-noi',
+    title: 'Hà Nội',
+    img: 'https://cf.bstatic.com/xdata/images/city/600x600/688853.jpg?k=f6427c8fccdf777e4bbc75fcd245e7c66204280181bea23350388c76c57348d1&o='
+  },
+  {
+    id: 'ho-chi-minh',
+    title: 'TP.Hồ Chí Minh',
+    img: 'https://cf.bstatic.com/xdata/images/city/600x600/688893.jpg?k=d32ef7ff94e5d02b90908214fb2476185b62339549a1bd7544612bdac51fda31&o='
+  }
+]
+
+export const secondaryLocations: LocationItem[] = [
+  {
+    id: 'da-nang',
+    title: 'Đà Nẵng',
+    img: 'https://cf.bstatic.com/xdata/images/city/600x600/688844.jpg?k=02892d4252c5e4272ca29db5faf12104004f81d13ff9db724371de0c526e1e15&o='
+  },
+  {
+    id: 'da-lat',
+    title: 'Đà Lạt',
+    img: 'https://cf.bstatic.com/xdata/images/city/600x600/688831.jpg?k=7b999c7babe3487598fc4dd89365db2c4778827eac8cb2a47d48505c97959a78&o='
+  },
+  {
+    id: 'vung-tau',
+    title: 'Vũng Tàu',
+    img: 'https://cf.bstatic.com/xdata/images/city/600x600/688956.jpg?k=fc88c6ab5434042ebe73d94991e011866b18ee486476e475a9ac596c79dce818&o='
+  }
+]
